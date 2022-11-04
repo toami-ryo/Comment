74bce97d9d2552b378839ae4a3fdd608ca2cf8a6
toami <tyungrw@gmail.com>
2022-11-04 11:15:17 +0900

    'add theme model'

--- mysite/comments/models.py
+++ mysite/comments/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+#Theme
+class Theme(models.Model):
+    title = models.CharField(max_length=50)
+
+
+
 # Cell : Cells of comment contents
 class Cell(models.Model):
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
