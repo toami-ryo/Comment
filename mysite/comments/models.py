a34bde0bb0f8b4daaf94b027e93b9634cbd8b4d7
toami <tyungrw@gmail.com>
2022-11-20 14:45:25 +0900

    'edit models'

--- mysite/comments/models.py
+++ mysite/comments/models.py
@@ -1,15 +1,33 @@
 from django.db import models
+from django.contrib import admin
+import datetime 
+from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
 #Theme
 class Theme(models.Model):
     title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField('data published')
+
+    def __str__(self):
+        return self.title
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?'
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
 # Cell : Cells of comment contents
 class Cell(models.Model):
     theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
-    comment_text = models.CharField(max_length=200)
+    comment_text = models.TextField(max_length=200)
     pub_date = models.DateTimeField('date published')
