769dd5dd75adfccd38b5b606cf200233d5830696
toami <tyungrw@gmail.com>
2022-11-22 17:11:16 +0900

    'edit texts'

--- mysite/comments/forms.py
+++ mysite/comments/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Cell
 
 class CommentContributeForm(forms.Form):
-    publisher_name = forms.CharField(label='名前')
-    comment_text = forms.CharField(widget=forms.Textarea(), max_length=200, label='コメント')
+    publisher_name = forms.CharField(label='名前　　 ')
+    comment_text = forms.CharField(widget=forms.Textarea(), max_length=200, label='コメント ')
 
 class ThemeContributeForm(forms.Form):
-    title = forms.CharField(label='タイトル')
-    publisher_name = forms.CharField(label='名前')
-    comment_zero = forms.CharField(widget=forms.Textarea(), max_length=200, label='コメント')
+    title = forms.CharField(label='タイトル ')
+    publisher_name = forms.CharField(label='名前　　 ')
+    comment_zero = forms.CharField(widget=forms.Textarea(), max_length=200, label='コメント ')
