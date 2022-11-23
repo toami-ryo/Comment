2a29d76897026d1cb9bd152f2878ad7fbc889653
toami <tyungrw@gmail.com>
2022-11-23 14:51:35 +0900

    'change back ground color, margin space and form'

--- mysite/comments/forms.py
+++ mysite/comments/forms.py
@@ -6,6 +6,6 @@ class CommentContributeForm(forms.Form):
     comment_text = forms.CharField(widget=forms.Textarea(), max_length=200, label='コメント ')
 
 class ThemeContributeForm(forms.Form):
-    title = forms.CharField(label='タイトル ')
-    publisher_name = forms.CharField(label='名前　　 ')
-    comment_zero = forms.CharField(widget=forms.Textarea(), max_length=200, label='コメント ')
+    title = forms.CharField(label='タイトル　 ')
+    publisher_name = forms.CharField(label='名前　　　 ')
+    comment_zero = forms.CharField(widget=forms.Textarea(), max_length=200, label='初コメント ')
