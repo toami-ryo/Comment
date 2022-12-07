768044490d9358da30e3c3f1a589e237d41fb6d6
toami <tyungrw@gmail.com>
2022-12-07 22:05:20 +0900

    'introduce django-environ'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+# useing django-environ
+# see https://www.taklog.info/python-django-envrion/
+import environ
+
+env = environ.ENV(
+    DEBUG=(bool, False)
+)
+
+env.read_env('.env')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +30,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0h(*pb7f@@80se#mgv(d!7ag*$j&wugy^hrn=3xx85hk2l@78'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
